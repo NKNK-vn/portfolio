7d70f72c41fc6c112a7c46d0e84cb05fc5284252
Khoi Nguyen <khoi@192.168.10.59>
2024-07-03 05:59:24 +0700

    add config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,12 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/portfolio/",
-  plugins: [react()],
+    base: '/portfolio/',
+    plugins: [react()],
+    server: {
+        port: 3000
+    },
+    css: {
+        devSourcemap: true
+    }
 })

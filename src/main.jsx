370a5c5ee4150e6a42a822c1fb2bcde002924317
Khoi Nguyen <khoi.nkn12@gmail.com>
2024-07-07 19:15:41 +0700

    add routes

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App'
 import { ThemeContextProvider } from './contexts/themeContexts'
+import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <ThemeContextProvider>
-            <App />
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>
         </ThemeContextProvider>
     </React.StrictMode>
 )
--- src/pages/Porfolio.jsx
+++ src/pages/Porfolio.jsx
@@ -1,16 +1,14 @@
 import { Button } from '@headlessui/react'
 import classNames from 'classnames'
-import React, { useContext, useState } from 'react'
-import HomePage from './HomePage'
+import PropTypes from 'prop-types'
+import React, { useContext } from 'react'
 import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react'
 import ThemeContext from '../contexts/themeContexts'
 import { Bars3Icon } from '@heroicons/react/24/outline'
-import AboutPage from './AboutPage'
 import Logo from '../assets/img/logo.png'
-import ResumePage from './ResumePage'
-import ContactPage from './ContactPage'
+import { NavLink } from 'react-router-dom'
 
-const Porfolio = () => {
+const Porfolio = ({ children }) => {
     const { isMobile } = useContext(ThemeContext)
     const TABS = {
         HOME: {
@@ -31,27 +29,12 @@ const Porfolio = () => {
         }
     }
 
-    const [isActiveTab, setIsActiveTab] = useState(TABS?.HOME?.tab)
-
-    const generatePageContent = () => {
-        let content = 'Updating ...'
-        if (isActiveTab === TABS?.HOME?.tab) {
-            content = <HomePage />
-        } else if (isActiveTab === TABS?.ABOUT?.tab) {
-            content = <AboutPage />
-        } else if (isActiveTab === TABS?.RESUME?.tab) {
-            content = <ResumePage />
-        } else if (isActiveTab === TABS?.CONTACT?.tab) {
-            content = <ContactPage />
-        }
-        return content
-    }
     return (
         <div>
             <div className='flex justify-between items-center'>
-                <div>
+                <NavLink to={`/portfolio`}>
                     <img src={Logo} alt='avatar' width={36} />
-                </div>
+                </NavLink>
                 <div>
                     {isMobile ? (
                         <Menu>
@@ -67,42 +50,49 @@ const Porfolio = () => {
                             <MenuItems anchor='bottom end' transition>
                                 {Object.keys(TABS)?.map((key) => (
                                     <MenuItem key={TABS?.[key]?.tab}>
-                                        <Button
-                                            className={classNames(
-                                                'my-1 rounded-xl bg-gray-400 py-2 px-4 text-sm text-white group flex w-full items-center',
-                                                {
-                                                    'bg-gradient-to-r from-red-600 to-rose-500':
-                                                        isActiveTab === TABS?.[key]?.tab
-                                                }
-                                            )}
+                                        <NavLink
+                                            to={`/portfolio/${TABS?.[key]?.tab}`}
+                                            end={TABS?.[key]?.tab === TABS?.HOME?.tab}
+                                            className={({ isActive }) => {
+                                                const activeClass = isActive
+                                                    ? 'bg-gradient-to-r from-red-600 to-rose-500'
+                                                    : ''
+                                                return `${activeClass} my-1 rounded-xl bg-gray-400 py-2 px-4 text-sm text-white group flex w-full items-center`
+                                            }}
                                             key={key?.title}
-                                            onClick={() => setIsActiveTab(TABS?.[key]?.tab)}
                                         >
                                             {TABS?.[key]?.title}
-                                        </Button>
+                                        </NavLink>
                                     </MenuItem>
                                 ))}
                             </MenuItems>
                         </Menu>
                     ) : (
-                        Object.keys(TABS)?.map((key) => (
-                            <Button
-                                className={classNames(
-                                    'mr-2 rounded-xl bg-gray-400 py-2 px-4 text-sm text-white data-[hover]:bg-gradient-to-r data-[hover]:from-red-600 data-[hover]:to-rose-500',
-                                    { 'bg-gradient-to-r from-red-600 to-rose-500': isActiveTab === TABS?.[key]?.tab }
-                                )}
-                                key={key?.title}
-                                onClick={() => setIsActiveTab(TABS?.[key]?.tab)}
-                            >
-                                {TABS?.[key]?.title}
-                            </Button>
-                        ))
+                        <div className='flex gap-2'>
+                            {Object.keys(TABS)?.map((key) => (
+                                <NavLink
+                                    to={`/portfolio/${TABS?.[key]?.tab}`}
+                                    // end={TABS?.[key]?.tab === TABS?.HOME?.tab}
+                                    className={({ isActive }) => {
+                                        const activeClass = isActive ? 'bg-gradient-to-r from-red-600 to-rose-500' : ''
+                                        return `${activeClass} my-1 rounded-xl bg-gray-400 py-2 px-4 text-sm text-white group flex w-full items-center`
+                                    }}
+                                    key={key?.title}
+                                >
+                                    {TABS?.[key]?.title}
+                                </NavLink>
+                            ))}
+                        </div>
                     )}
                 </div>
             </div>
-            <div className='mt-20'>{generatePageContent()}</div>
+            <div className='mt-20'>{children}</div>
         </div>
     )
+}
+
+Porfolio.propTypes = {
+    children: PropTypes.oneOfType([PropTypes.arrayOf(PropTypes.node), PropTypes.node]).isRequired
 }
 
 export default Porfolio
